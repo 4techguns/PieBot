e5512d70c3f6807a3f9f2e3d6056cdaf3afedcea
4TechGuns <fourtechgunstest@outlook.com>
2022-06-11 19:41:24 -0230

    (unstable) code splitting

--- src/main/kotlin/com/github/fourtechguns/bot/Main.kt
+++ src/main/kotlin/com/github/fourtechguns/bot/Main.kt
@@ -11,7 +11,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 import java.util.*
 
 fun main(args: Array<String>) {
-val shhhhhhhhhhhhhhhhhhhhhhhhhh: Properties = PropsManager.getProps("/conf/bot_secrets.properties")
+    val shhhhhhhhhhhhhhhhhhhhhhhhhh: Properties = PropsManager.getProps("/conf/bot_secrets.properties")
     val botconf: Properties = PropsManager.getProps("/conf/bot.properties")
 
     var jda = default(shhhhhhhhhhhhhhhhhhhhhhhhhh.getProperty("token"), enableCoroutines = true)
@@ -21,42 +21,6 @@ val shhhhhhhhhhhhhhhhhhhhhhhhhh: Properties = PropsManager.getProps("/conf/bot_s
             GatewayIntent.GUILD_MESSAGES
         )
     }
-        .awaitReady()
-
-    jda.presence.setPresence(
-        Activity.of(
-            Activity.ActivityType.COMPETING,
-            "${jda.guilds.size} guilds"),
-        false)
-
-    jda.listener<MessageReceivedEvent> {
-        if (it.message.referencedMessage != null && it.message.contentRaw == "ratio")
-        {
-            it.message.addReaction("❤").queue()
-        }
-    }
-
-    jda.listener<MessageReactionAddEvent> {
-        if (it.user?.isBot == true) return@listener
-
-        when (it.reactionEmote.emoji)
-        {
-            "❤" -> {
-                it.retrieveMessage()
-                    .queue {m ->
-                        m.reactions.find { it.reactionEmote.emoji == "❤" }!!.retrieveUsers()
-                            .queue {
-                                r ->
-                                if (r.size > 10) {
-                                    m.channel.sendMessage("${m.author.asMention}, you have successfully ratioed ${m.referencedMessage!!.author.asMention}")
-                                        .queue()
-                                    m.delete().queue()
-
-                                }
-
-                            }
-                    }
-            }
-        }
-    }
+    val pie: Pie = Pie(jda)
+    pie.start()
 }
